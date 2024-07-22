adda30d842e693ac9a80a995d51ee1847fabff62
Alessandro Lollo <alessandro.lollo@cloudacademy.com>
2024-07-22 10:17:32 +0200

    changed permissions to ps1 scripts

--- debezium/azure/01-register-connector.sh
+++ debezium/azure/01-register-connector.sh
@@ -3,8 +3,8 @@
 # Strict mode, fail on any error
 set -euo pipefail
 
-RESOURCE_GROUP="dbzrg"
-CONTAINER_NAME="dbzcontainer"
+RESOURCE_GROUP="debezium"
+CONTAINER_NAME="debezium"
 
 echo "finding debezium ip"
 DEBEZIUM_IP=`az container show --resource-group $RESOURCE_GROUP --name $CONTAINER_NAME --output tsv --query "ipAddress.ip"`
